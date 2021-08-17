94f689bf144ed55dfe8d9cfe5a1cd284efe85740
Felix Weilbach <felix.weilbach@nextcloud.com>
2021-08-17 09:13:48 +0200

    Use build-appimage.sh from Brander
    Signed-off-by: Felix Weilbach <felix.weilbach@nextcloud.com>

--- admin/linux/build-appimage.sh
+++ admin/linux/build-appimage.sh
@@ -2,8 +2,15 @@
 
 set -xe
 
+export APPNAME=${APPNAME:-nextcloud}
+export BUILD_UPDATER=${BUILD_UPDATER:-OFF}
+export BUILDNR=${BUILDNR:-0000}
+
 mkdir /app
 mkdir /build
+
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
 
 #Set Qt-5.12
 export QT_BASE_DIR=/opt/qt5.12.10
@@ -12,16 +19,12 @@ export PATH=$QT_BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
-#Set APPID for .desktop file processing
-export LINUX_APPLICATION_ID=com.nextcloud.desktopclient.nextcloud
-
-#set defaults
-export SUFFIX=${DRONE_PULL_REQUEST:=master}
-if [ $SUFFIX != "master" ]; then
-    SUFFIX="PR-$SUFFIX"
+# Set defaults
+if [ "$BUILD_UPDATER" != "OFF" ]; then
+    BUILD_UPDATER=ON
 fi
 
-#QtKeyChain v0.10.0
+#QtKeyChain master
 cd /build
 git clone https://github.com/frankosterfeld/qtkeychain.git
 cd qtkeychain
@@ -29,36 +32,39 @@ git checkout v0.10.0
 mkdir build
 cd build
 cmake -D CMAKE_INSTALL_PREFIX=/usr ../
-make -j4
-make install
+make -j$(nproc)
+make -j$(nproc) DESTDIR=/app install
+make -j$(nproc) install
+
 
 #Build client
 cd /build
 mkdir build-client
 cd build-client
 cmake -D CMAKE_INSTALL_PREFIX=/usr \
+    -D NO_SHIBBOLETH=1 \
     -D BUILD_TESTING=OFF \
-    -D BUILD_UPDATER=ON \
-    -DMIRALL_VERSION_SUFFIX=PR-$DRONE_PULL_REQUEST \
-    -DMIRALL_VERSION_BUILD=$DRONE_BUILD_NUMBER \
-    $DRONE_WORKSPACE
-make -j4
-make DESTDIR=/app install
+    -D BUILD_UPDATER=$BUILD_UPDATER \
+    -D QTKEYCHAIN_LIBRARY=/app/usr/lib/x86_64-linux-gnu/libqt5keychain.so \
+    -D QTKEYCHAIN_INCLUDE_DIR=/app/usr/include/qt5keychain/ \
+    -D MIRALL_VERSION_BUILD=$BUILDNR \
+    /home/user/
+make -j$(nproc)
+make -j$(nproc) DESTDIR=/app install
 
 # Move stuff around
 cd /app
 
 mv ./usr/lib/x86_64-linux-gnu/* ./usr/lib/
+
 mkdir ./usr/plugins
 mv ./usr/lib/nextcloudsync_vfs_suffix.so ./usr/plugins
 mv ./usr/lib/nextcloudsync_vfs_xattr.so ./usr/plugins
+
 rm -rf ./usr/lib/cmake
 rm -rf ./usr/include
 rm -rf ./usr/mkspecs
 rm -rf ./usr/lib/x86_64-linux-gnu/
-
-# Don't bundle nextcloudcmd as we don't run it anyway
-rm -rf ./usr/bin/nextcloudcmd
 
 # Don't bundle the explorer extentions as we can't do anything with them in the AppImage
 rm -rf ./usr/share/caja-python/
@@ -66,10 +72,11 @@ rm -rf ./usr/share/nautilus-python/
 rm -rf ./usr/share/nemo-python/
 
 # Move sync exclude to right location
-mv ./etc/Nextcloud/sync-exclude.lst ./usr/bin/
+mv ./etc/*/sync-exclude.lst ./usr/bin/
 rm -rf ./etc
 
-DESKTOP_FILE=/app/usr/share/applications/${LINUX_APPLICATION_ID}.desktop
+# com.nextcloud.desktopclient.nextcloud.desktop
+DESKTOP_FILE=$(ls /app/usr/share/applications/*.desktop)
 sed -i -e 's|Icon=nextcloud|Icon=Nextcloud|g' ${DESKTOP_FILE} # Bug in desktop file?
 cp ./usr/share/icons/hicolor/512x512/apps/Nextcloud.png . # Workaround for linuxeployqt bug, FIXME
 
@@ -85,18 +92,19 @@ cp -P -r /usr/lib/x86_64-linux-gnu/nss ./usr/lib/
 
 # Use linuxdeployqt to deploy
 cd /build
-wget --ca-directory=/etc/ssl/certs/ -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+wget --ca-directory=/etc/ssl/certs -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt*.AppImage
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract
 rm ./linuxdeployqt-continuous-x86_64.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/app/usr/lib/
-./squashfs-root/AppRun ${DESKTOP_FILE} -bundle-non-qt-libs -qmldir=$DRONE_WORKSPACE/src/gui
+./squashfs-root/AppRun ${DESKTOP_FILE} -bundle-non-qt-libs -qmldir=/home/user/src/gui
 
 # Set origin
-./squashfs-root/usr/bin/patchelf --set-rpath '$ORIGIN/' /app/usr/lib/libnextcloudsync.so.0
+./squashfs-root/usr/bin/patchelf --set-rpath '$ORIGIN/' /app/usr/lib/lib${APPNAME}sync.so.0
 
 # Build AppImage
 ./squashfs-root/AppRun ${DESKTOP_FILE} -appimage
 
-mv Nextcloud*.AppImage Nextcloud-${SUFFIX}-${DRONE_COMMIT}-x86_64.AppImage
+#move AppImage
+mv /build/*.AppImage /home/user/
